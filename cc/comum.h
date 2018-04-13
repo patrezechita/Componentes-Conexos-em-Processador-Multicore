15194d396ae4ee4e07b47f4c8a0cdae2ff471ed5
Patreze Chita <patrezechita@outlook.com>
2018-04-13 00:37:22 -0400

    qtd de cc

--- cc/comum.h
+++ cc/comum.h
@@ -7,18 +7,18 @@ int **lerEntrada() {
 	fscanf(arqEntrada, "%d %d", &qtdVertices, &qtdArestas);
 	qtdArestas++;
 
-    // aloca memoria para a matriz Nx2
-    matrizEntrada = malloc(qtdArestas * sizeof(int *));
-    for (int i = 0; i < qtdArestas; i++) {
+	// aloca memoria para a matriz Nx2
+	matrizEntrada = malloc(qtdArestas * sizeof(int *));
+	for (int i = 0; i < qtdArestas; i++) {
 		matrizEntrada[i] = malloc(2 * sizeof(int));
-    }
+	}
 
-    // coloca de volta a primeira linha
-    matrizEntrada[0][0] = qtdVertices;
-    matrizEntrada[0][1] = qtdArestas;
+	// coloca de volta a primeira linha
+	matrizEntrada[0][0] = qtdVertices;
+	matrizEntrada[0][1] = qtdArestas;
 
-    // le o resto do arquivo
-    for(int i=1; i<qtdArestas; i++) {
+	// le o resto do arquivo
+	for(int i=1; i<qtdArestas; i++) {
 		fscanf(arqEntrada, "%d %d", &matrizEntrada[i][0], &matrizEntrada[i][1]);
 	}
 
@@ -27,9 +27,31 @@ int **lerEntrada() {
 
 void imprimeSaida(char nomeSaida[], int vetorSaida[], int tamVetor) {
 	FILE *arqSaida;
-  	arqSaida = fopen (nomeSaida,"w");
+	arqSaida = fopen (nomeSaida,"w");
+	int vetorQtdCC[tamVetor], cc;
 
-  	// imprime o vetor no arquivo de saida
+	// preenche o vetor auxiliar com 0
+	for(int i=0; i<tamVetor; i++) {
+		vetorQtdCC[i]=0;
+	}
+
+	// da 1 para cada indice que achar
+	for(int i=0; i<tamVetor; i++) {
+		vetorQtdCC[vetorSaida[i]]=1;
+	}
+
+	// conta os indices, sera o numero de componentes
+	cc = 0;
+	for(int i=0; i<tamVetor; i++) {
+		if(vetorQtdCC[i]==1) {
+			cc++;
+		}
+	}
+
+	// imprime a quantidade de componentes
+	fprintf(arqSaida, "%d\n", cc);
+
+	// imprime o vetor no arquivo de saida
 	for(int i=0; i<tamVetor; i++) {
 		fprintf(arqSaida, "%d %d\n", i, vetorSaida[i]);
 	}
